445c854efb3bff212490ea99f37beed595c654b4
Nicolay Ramm <nicolayr@ifi.uio.no>
2015-12-15 12:41:23 +0100

    Add proxy for localhost:8080 to webpack dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,17 @@ webpackBaseConfig.devServer = {
     colors: true,
     port: 8081,
     inline: true,
+    proxy: {
+        '/api/*': { target: 'http://localhost:8080' },
+        '/dhis-web-commons/*': { target: 'http://localhost:8080' },
+        '/icons/*': { target: 'http://localhost:8080' },
+        '/browser-polyfill.min.js': {
+            target: 'http://localhost:8081/node_modules/babel-core/',
+        },
+        '/jquery.min.js': {
+            target: 'http://localhost:8081/node_modules/jquery/dist/',
+        },
+    },
 };
 
 module.exports = webpackBaseConfig;
