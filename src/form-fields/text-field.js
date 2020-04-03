c902d8ff23b921dc4acc9d79ae71d88b4b9e33af
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2020-04-03 11:12:54 +0200

    fix: disable untranslated localized fields and add help texts (v31) (#493)
    * fix: disable untranslated localized fields and add help texts (v31)
    
    * chore: fix build by not using arrow methods in class properties

--- src/form-fields/text-field.js
+++ src/form-fields/text-field.js
@@ -1,42 +1,56 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 
-import MuiThemeMixin from '../mui-theme.mixin';
+const helpTextStyle = {
+    fontSize: '12px',
+    color: 'rgba(0, 0, 0, 0.3)',
+    margin: '-4px 0 0',
+}
 
+class TextFieldComponent extends React.Component {
+    constructor(props) {
+        super(props);
 
-// TODO: Rewrite as ES6 class
-/* eslint-disable react/prefer-es6-class */
-export default React.createClass({
-    propTypes: {
-        value: React.PropTypes.string,
-        multiLine: React.PropTypes.bool,
-    },
-
-    mixins: [MuiThemeMixin],
-
-    getInitialState() {
-        return {
-            value: this.props.value,
-        };
-    },
+        this.onChange = this.onChange.bind(this);
+        this.state = {
+            value: props.value,
+        }
+    }
 
     componentWillReceiveProps(props) {
         this.setState({ value: props.value });
-    },
+    }
 
     onChange(e) {
         this.setState({ value: e.target.value });
-    },
+    }
 
     render() {
-        const { changeEvent, isRequired, defaultValue, ...other } = this.props; // eslint-disable-line
+        const { changeEvent, isRequired, defaultValue, helpText, ...other } = this.props; // eslint-disable-line
         const errorStyle = {
             lineHeight: this.props.multiLine ? '48px' : '12px',
             marginTop: this.props.multiLine ? -16 : 0,
         };
 
         return (
+          <div>
             <TextField errorStyle={errorStyle} {...other} value={this.state.value} onChange={this.onChange} />
+            {helpText && <p style={helpTextStyle}>{helpText}</p>}
+          </div>
         );
-    },
-});
+    }
+}
+
+TextFieldComponent.propTypes = {
+    value: React.PropTypes.string,
+    multiLine: React.PropTypes.bool,
+    helpText: React.PropTypes.node,
+}
+
+TextFieldComponent.defaultProps = {
+    value: '',
+    multiLine: false,
+    helpText: '',
+}
+
+export default TextFieldComponent;
