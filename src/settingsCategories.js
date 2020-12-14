fbec09dccc45b188e8fcc62bcb872fbde4b591a4
Birk Johansson <birk@dhis2.org>
2020-12-14 15:03:20 +0100

    feat(analytics): remove skip zero values in analytics tables-setting

--- src/settingsCategories.js
+++ src/settingsCategories.js
@@ -49,7 +49,6 @@ export const categories = {
             'keyIgnoreAnalyticsApprovalYearThreshold',
             'keyRespectMetaDataStartEndDatesInAnalyticsTableExport',
             'keyAnalyticsMaintenanceMode',
-            'keySkipZeroValuesInAnalyticsTableExport',
         ],
     },
     server: {
