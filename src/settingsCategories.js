242a0aed4ba9c12dc9c6a8b8adf804267dfd1699
Thomas Zemp <tzemp@baosystems.com>
2021-02-19 17:45:18 +0100

    feat: add keyCountPassiveDashboardViewsInUsageAnalytics [DHIS2-7016]
    * adds keyCountPassiveDashboardViewsInUsageAnalytics under General

--- src/settingsCategories.js
+++ src/settingsCategories.js
@@ -30,6 +30,7 @@ export const categories = {
             'multiOrganisationUnitForms',
             'keyAcceptanceRequiredForApproval',
             'keyGatherAnalyticalObjectStatisticsInDashboardViews',
+            'keyCountPassiveDashboardViewsInUsageAnalytics',
         ],
     },
     analytics: {
