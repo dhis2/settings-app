7c317d08ab9182d221fd5451531602e9b925e704
Thomas Zemp <tom@dhis2.org>
2023-03-01 10:45:58 +0100

    fix: add warning for calendar update [DHIS2-7422] (#1120)

--- src/form-fields/drop-down.js
+++ src/form-fields/drop-down.js
@@ -1,3 +1,11 @@
+import {
+    Button,
+    ButtonStrip,
+    Modal,
+    ModalContent,
+    ModalActions,
+    ModalTitle,
+} from '@dhis2/ui'
 import MenuItem from 'material-ui/MenuItem'
 import SelectField from 'material-ui/SelectField'
 import PropTypes from 'prop-types'
@@ -15,6 +23,7 @@ class DropDown extends React.Component {
         includeEmpty: PropTypes.bool,
         menuItems: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
         noOptionsLabel: PropTypes.string,
+        warning: PropTypes.object,
         onBlur: PropTypes.func,
         onChange: PropTypes.func,
         onFocus: PropTypes.func,
@@ -26,6 +35,7 @@ class DropDown extends React.Component {
         emptyLabel: '',
         menuItems: [],
         noOptionsLabel: '',
+        warning: undefined,
 
         onFocus: undefined,
         onBlur: undefined,
@@ -34,11 +44,28 @@ class DropDown extends React.Component {
 
     constructor(props) {
         super(props)
-
+        this.state = { warningModalOpen: false, pendingValue: null }
         this.handleChange = this.handleChange.bind(this)
+        this.closeWarningModal = this.closeWarningModal.bind(this)
+        this.confirmProceedAfterWarning =
+            this.confirmProceedAfterWarning.bind(this)
     }
 
     handleChange(event, index, value) {
+        if (this.props.warning) {
+            this.setState({ warningModalOpen: true, pendingValue: value })
+        } else {
+            this.props.onChange({ target: { value } })
+        }
+    }
+
+    closeWarningModal() {
+        this.setState({ warningModalOpen: false, pendingValue: null })
+    }
+
+    confirmProceedAfterWarning() {
+        const value = this.state.pendingValue
+        this.setState({ warningModalOpen: false, pendingValue: null })
         this.props.onChange({ target: { value } })
     }
 
@@ -75,26 +102,50 @@ class DropDown extends React.Component {
             emptyLabel,
             noOptionsLabel,
             isRequired,
+            warning,
             /* eslint-enable no-unused-vars, react/prop-types */
             ...other
         } = this.props
         const hasOptions = menuItems.length > 0
         return (
-            <SelectField
-                value={hasOptions ? this.props.value : 1}
-                onChange={this.handleChange}
-                disabled={!hasOptions || disabled}
-                {...other}
-            >
-                {hasOptions ? (
-                    this.renderMenuItems(menuItems)
-                ) : (
-                    <MenuItem
-                        value={1}
-                        primaryText={this.props.noOptionsLabel || '-'}
-                    />
+            <>
+                {this.state.warningModalOpen && (
+                    <Modal onClose={this.closeWarningModal}>
+                        <ModalTitle>{this.props.warning.title}</ModalTitle>
+                        <ModalContent>{this.props.warning.body}</ModalContent>
+
+                        <ModalActions>
+                            <ButtonStrip end>
+                                <Button onClick={this.closeWarningModal}>
+                                    {this.props.warning.cancel}
+                                </Button>
+                                <Button
+                                    destructive
+                                    onClick={this.confirmProceedAfterWarning}
+                                >
+                                    {this.props.warning.proceed}
+                                </Button>
+                            </ButtonStrip>
+                        </ModalActions>
+                    </Modal>
                 )}
-            </SelectField>
+
+                <SelectField
+                    value={hasOptions ? this.props.value : 1}
+                    onChange={this.handleChange}
+                    disabled={!hasOptions || disabled}
+                    {...other}
+                >
+                    {hasOptions ? (
+                        this.renderMenuItems(menuItems)
+                    ) : (
+                        <MenuItem
+                            value={1}
+                            primaryText={this.props.noOptionsLabel || '-'}
+                        />
+                    )}
+                </SelectField>
+            </>
         )
     }
 }
