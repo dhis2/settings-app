6ffd71e2118351da32138f75a40e4b37fb377fe4
Mozafar Haider <mozafar@dhis2.org>
2024-03-13 16:01:27 +0000

    fix: update app name to match the bundled apps in core

--- d2.config.js
+++ d2.config.js
@@ -1,7 +1,7 @@
 const config = {
     id: '4f6eab1a-29ac-4f7a-b74c-8ba95ea7df49',
     type: 'app',
-    name: 'System Settings',
+    name: 'settings',
     title: 'System Settings',
     coreApp: true,
     minDHIS2Version: '2.41',
