2db42222b4cefe650b48097b024ef24e249cb22e
Diana Nanyanzi <31903212+d-rita@users.noreply.github.com>
2025-09-25 14:31:18 +0200

    feat: add shortcuts for command palette (#1447)
    * feat: add shortcuts to command palette
    
    - update cli-app-scripts dependency
    - add shortcuts and names to d2 config file
    - update translations
    
    * fix: spelling error
    
    * chore: update translations
    
    * feat: add shortcut for notification settings

--- d2.config.js
+++ d2.config.js
@@ -1,3 +1,4 @@
+/**@type {import('@dhis2/cli-app-scripts').D2Config} */
 const config = {
     id: '4f6eab1a-29ac-4f7a-b74c-8ba95ea7df49',
     type: 'app',
@@ -9,6 +10,56 @@ const config = {
     entryPoints: {
         app: './src/App.jsx',
     },
+    shortcuts: [
+        {
+            name: 'General',
+            url: '#/general',
+        },
+        {
+            name: 'Analytics',
+            url: '#/analytics',
+        },
+        {
+            name: 'Server',
+            url: '#/server',
+        },
+        {
+            name: 'Appearance',
+            url: '#/appearance',
+        },
+        {
+            name: 'Email',
+            url: '#/email',
+        },
+        {
+            name: 'Access',
+            url: '#/access',
+        },
+        {
+            name: 'Calendar',
+            url: '#/calendar',
+        },
+        {
+            name: 'Data import settings',
+            url: '#/import',
+        },
+        {
+            name: 'Synchronization',
+            url: '#/sync',
+        },
+        {
+            name: 'Scheduled job settings',
+            url: '#/scheduledJobs',
+        },
+        {
+            name: 'Notification settings',
+            url: '#/notification',
+        },
+        {
+            name: 'OAuth2 clients',
+            url: '#/oauth2',
+        },
+    ],
 }
 
 module.exports = config
